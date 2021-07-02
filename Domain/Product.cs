4f406de1a0622b153771795a1ffe2a6050fb5bd2
18127006 <18127006@student.hcmus.edu.vn>
2021-07-02 15:29:15 +0700

    (ADD) Entities: Product, ProductImage, Category, SubCategory. Enum: ProductEnum

---
+++ Domain/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain
+{
+    public class Product
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Stock { get; set; }
+
+        public List<ProductImage> Images { get; set; }
+
+        public SubCategory SubCategory { get; set; }
+    }
+}
---
+++ Domain/SubCategory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain
+{
+    public class SubCategory
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public Category Category { get; set; }
+    }
+}
